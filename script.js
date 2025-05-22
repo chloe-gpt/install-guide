a9cf684fe640728710d62eb8ec020fff8ad6e7cb
Glitch (town-of-us-install-guide) <none>
2025-05-22 19:32:37 +0000

    🎛️🥰 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function copyCode(codeBlockId, button) {
+  const code = document.getElementById(codeBlockId).innerText;
+  navigator.clipboard.writeText(code).then(() => {
+    const originalText = button.innerText;
+    button.innerText = 'Code copied!';
+    setTimeout(() => {
+      button.innerText = originalText;
+    }, 1500); // Revert after 1.5 seconds
+  });
+}
